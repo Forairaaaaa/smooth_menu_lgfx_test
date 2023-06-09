de71f2fd88cefb8e2041cd7baf71169c3030199f
Forairaaaaa <applesyqd@outlook.com>
2023-06-09 18:08:41 +0800

    lv wrap test ok

--- main.cpp
+++ main.cpp
@@ -41,6 +41,80 @@ int Game_random(int low, int high)
 
 
 
+
+
+
+
+
+
+
+
+#include "smooth_menu/src/lv_anim/lv_anim.h"
+
+
+LVGL::Anim_Path test;
+LVGL::Anim_Path test2;
+
+
+void smooth_menu_test()
+{
+    test.setAnim(LVGL::bounce, 0, 150, 1000);
+    test.resetTime(SDL_GetTicks());
+
+    test2.setAnim(LVGL::ease_out, 0, 40, 400);
+    test2.resetTime(SDL_GetTicks());
+
+
+    while (1) {
+
+        _canvas->clear();
+
+        _canvas->fillSmoothCircle(test.getValue(SDL_GetTicks()) + 10, 20, 10, TFT_YELLOW);
+
+        _canvas->fillRect(10, 50, test.getValue(SDL_GetTicks()), 10, TFT_GREEN);
+
+        _canvas->fillSmoothRoundRect(10, test2.getValue(SDL_GetTicks()) + 50, 80, 26, 3, TFT_ORANGE);
+
+
+        test.setAnim(LVGL::overshoot, 0, 100, 600);
+        test2.setAnim(LVGL::overshoot, 0, 50, 600);
+        _canvas->fillSmoothRoundRect(110, test2.getValue(SDL_GetTicks()) + 30, test.getValue(SDL_GetTicks()), 26, 3, TFT_ORANGE);
+        test.setAnim(LVGL::bounce, 0, 150, 1000);
+        test2.setAnim(LVGL::ease_out, 0, 40, 400);
+
+
+        _canvas_update();
+
+
+
+
+
+
+        if (lgfx::gpio_in(PIN_UP) == 0) {
+            test.resetTime(SDL_GetTicks());
+            test2.resetTime(SDL_GetTicks());
+        }
+
+
+    } 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void setup()
 {
     lcd.init();
@@ -56,8 +130,9 @@ void setup()
 }
 
 
+
 void loop()
 {
-
+    smooth_menu_test();
 }
 
